993275a9c8fffe0517a0539807c0534a669783a7
lvenchi <venchilorenzo@gmail.com>
2019-12-22 16:24:17 +0100

    gitignore update

--- app/src/main/java/com/example/alarmlocation/RemoveAlarmAcrivity.kt
+++
@@ -1,4 +0,0 @@
-package com.example.alarmlocation
-
-class RemoveAlarmAcrivity {
-}
---
+++ app/src/main/java/com/example/alarmlocation/RemoveAlarmActivity.kt
@@ -0,0 +1,6 @@
+package com.example.alarmlocation
+
+import androidx.appcompat.app.AppCompatActivity
+
+class RemoveAlarmActivity : AppCompatActivity() {
+}
